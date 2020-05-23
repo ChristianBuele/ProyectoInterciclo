61e1cf9c2e772a363431ffd5ab5d8dbd3425c00e
Jonnathan Valdez <jonnathan.valdez@ucuenca.edu.ec>
2020-05-22 21:25:24 -0500

    interfaz

---
+++ src/Vista/principal.java
@@ -0,0 +1,11 @@
+package Vista;
+
+import javax.swing.*;
+
+public class principal {
+    private JTabbedPane tabbedPane1;
+    private JTextArea textArea1;
+    private JFormattedTextField formattedTextField1;
+    private JFormattedTextField formattedTextField2;
+    private JButton calcularButton;
+}
